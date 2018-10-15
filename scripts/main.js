54a1d8cade235ba868154e936018107fd18b02ae
zhuzini <zinizhu@berkeley.edu>
2018-10-15 13:49:20 -0700

    Homepage Finished

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	/* FIXED NAV BAR */
+	var $header = $('header');
+    var $sticky = $header.before($header.clone().addClass("sticky"));
+
+    $(window).on("scroll", function(){
+      var scrollFromTop = $(window).scrollTop();
+      $("body").toggleClass("scroll", (scrollFromTop > 30));
+    });
+});
