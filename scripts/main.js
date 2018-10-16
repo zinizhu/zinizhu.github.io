5f052e9c1560dd7d073b85d9f8f4788f8f15e6f9
zhuzini <zinizhu@berkeley.edu>
2018-10-15 19:41:35 -0700

    HomePage Modified1
    Menu bar is now size-related.

--- scripts/main.js
+++ scripts/main.js
@@ -8,4 +8,24 @@ $(document).ready(function() {
       var scrollFromTop = $(window).scrollTop();
       $("body").toggleClass("scroll", (scrollFromTop > 30));
     });
+
+
+    //RESPONSIVE MENU START
+
+      var body = $('body');
+      var menuTrigger = $('.js_menu_trigger');
+      var mainOverlay = $('.js_main_overlay');
+
+      menuTrigger.on('click', function(){
+        body.addClass('menu-is-active');
+      });
+
+      mainOverlay.on('click', function(){
+        body.removeClass('menu-is-active');
+      });
+
+      $('.menu li a').on('click', function(){
+          $('body').removeClass("menu-is-active");
+      });
+
 });
